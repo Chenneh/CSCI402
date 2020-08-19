50ece3273e7af7b2632bbf8ffcd02500875e5775
Chen <whitebai1221@gmail.com>
2020-08-18 23:12:41 -0700

    more implementation

--- HW1/my402list.c
+++ HW1/my402list.c
@@ -48,13 +48,44 @@ extern int  My402ListInsertAfter(My402List*, void*, My402ListElem*);
 
 extern int  My402ListInsertBefore(My402List*, void*, My402ListElem*);
 
+My402ListElem *My402ListFirst(My402List* list) {
+    if (list->num_members == 0) {
+        return NULL;
+    }
+    return list->anchor.next;
+}
 
-extern My402ListElem *My402ListFirst(My402List*);
-extern My402ListElem *My402ListLast(My402List*);
-extern My402ListElem *My402ListNext(My402List*, My402ListElem*);
-extern My402ListElem *My402ListPrev(My402List*, My402ListElem*);
+My402ListElem *My402ListLast(My402List* list) {
+    if (list->num_members == 0) {
+        return NULL;
+    }
+    return list->anchor.prev;
+}
 
-extern My402ListElem *My402ListFind(My402List*, void*);
+My402ListElem *My402ListNext(My402List* list, My402ListElem* cur) {
+    if(cur->next == &(list->anchor)) {
+        return NULL;
+    }
+    return cur->next;
+}
+
+My402ListElem *My402ListPrev(My402List* list, My402ListElem* cur) {
+    if (cur->next == &(list->anchor)) {
+        return NULL;
+    }
+    return cur->prev;
+}
+
+My402ListElem *My402ListFind(My402List* list, void* obj) {
+    My402ListElem *cur = list->anchor.next;
+    while (cur != &(list->anchor)) {
+        if (cur->obj == obj) {
+            return cur;
+        }
+        cur = cur->next;
+    }
+    return NULL;
+}
 
 int My402ListInit(My402List* list) {
     list->num_members = 0;
