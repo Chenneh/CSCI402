65e2664d7df8d10e23d7483b00efeac00a513a2b
Chen <whitebai1221@gmail.com>
2020-08-18 21:47:34 -0700

    basic implementation

---
+++ HW1/my402list.c
@@ -0,0 +1,65 @@
+#include "my402list.h"
+
+int  My402ListLength(My402List* list) {
+    return list->num_members;
+}
+
+int My402ListEmpty(My402List* list) {
+    if (list->num_members == 0) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+int  My402ListAppend(My402List* list, void* obj) {
+    My402ListElem* newNode = (My402ListElem*)malloc(sizeof(My402ListElem));
+    if (newNode == NULL) {
+        return FALSE;
+    }
+    newNode->obj = obj;
+    newNode->next = &(list->anchor);
+    newNode->prev = list->anchor.prev;
+    // diff
+    list->anchor.prev->next = newNode;
+    list->anchor.prev = newNode;
+    list->num_members = list->num_members + 1;
+}
+
+int  My402ListPrepend(My402List* list, void* obj) {
+    My402ListElem* newNode = (My402ListElem*)malloc(sizeof(My402ListElem));
+    if (newNode == NULL) {
+        return FALSE;
+    }
+    newNode->obj = obj;
+    newNode->next = list->anchor.next;
+    newNode->prev = &(list->anchor);
+    // diff
+    list->anchor.next->prev = newNode;
+    list->anchor.next = newNode;
+    list->num_members = list->num_members + 1;
+}
+
+extern void My402ListUnlink(My402List*, My402ListElem*);
+
+extern void My402ListUnlinkAll(My402List*);
+
+extern int  My402ListInsertAfter(My402List*, void*, My402ListElem*);
+
+extern int  My402ListInsertBefore(My402List*, void*, My402ListElem*);
+
+
+extern My402ListElem *My402ListFirst(My402List*);
+extern My402ListElem *My402ListLast(My402List*);
+extern My402ListElem *My402ListNext(My402List*, My402ListElem*);
+extern My402ListElem *My402ListPrev(My402List*, My402ListElem*);
+
+extern My402ListElem *My402ListFind(My402List*, void*);
+
+int My402ListInit(My402List* list) {
+    list->num_members = 0;
+    list->anchor.obj = NULL;
+    list->anchor.next = &(list->anchor);
+    list->anchor.prev = &(list->anchor);
+    return TRUE;
+}
